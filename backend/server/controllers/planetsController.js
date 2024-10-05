f0e25809b88ae3701b83ca4f99f27d4a4bf35b16
PESolut <jahaadpetty@pursuit.org>
2024-10-05 15:55:00 -0400

    get all planets working

--- backend/server/controllers/planetsController.js
+++ backend/server/controllers/planetsController.js
@@ -3,9 +3,21 @@ const router = express.Router();
 const { fetchPlanetVectorData } = require('../../utils/planets');
 
 // GET all planets
-router.get('/', (req, res) => {
-  const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-  res.json(planets);
+router.get('/', async (req, res) => {
+    try {
+        const arr = []
+        const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+        for (const planet of planets) {
+          console.log(planet)
+          const vectorData = await fetchPlanetVectorData(planet);
+          arr.push(vectorData)
+        }
+        res.json(arr);
+
+    } catch {
+
+    }
+   
 });
 
 // GET vector data for a specific planet
